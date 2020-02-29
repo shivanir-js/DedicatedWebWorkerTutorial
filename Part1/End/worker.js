1b2b52c5f8e630e9ed1127674eb405b4744c409d
shivanir-js <shivanir-js@users.noreply.github.com>
2020-02-29 17:29:21 +0530

    Add Logs and ParseInt

--- Part1/End/worker.js
+++ Part1/End/worker.js
@@ -1,6 +1,6 @@
 onmessage = function(event){
     console.log("Worker: Message received from the main script");
-    result = event.data[0] * event.data[1];
+    result = parseInt(event.data[0]) + parseInt(event.data[1]);
     if(isNaN(result)){
         console.log("Worker: Please write two numbers")
     }
